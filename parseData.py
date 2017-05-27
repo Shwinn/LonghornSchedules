3fb5883d16d0e2e9f1e8c49cf06ebe6a22b9050e
Avishka Suduwa Dewage <avishka@utexas.edu>
2017-05-27 14:54:23 -0500

    gets full directory and parses and writes data to test file"

--- parseData.py
+++ parseData.py
@@ -1,9 +1,12 @@
 from bs4 import BeautifulSoup
 
-data = open("data.txt", 'r')
+print("opening data file...")
+data = open("completeData.txt", 'r')
 
+print("creating beutiful soup object...")
 soup = BeautifulSoup(data, 'html.parser')
 
+print("opening file to write...")
 test = open("test.txt" , 'w')
 
 #arrays of data
@@ -19,80 +22,85 @@ classInstructor = []
 classStatus = []
 
 #this array has everything that we need --course name, days, times
-tableBody = soup.tbody
-courseInfo = tableBody.find_all('tr')
+courseInfo = soup.find_all('tr')
 
-classIndex = 0
+courseInfoDataLength = len(courseInfo)
 
 for x in range(len(courseInfo)-1):
-	classAttribute = courseInfo[x].td.get('class')
-	if(classAttribute != None):
-		if(classAttribute[0] == 'course_header'):
-			classNameIndex = x;
-			x += 1
+	print("working " + str(x) + " of " + str(courseInfoDataLength))
+	if(courseInfo[x].td != None):
 
-			while(True):
-				className.append(courseInfo[classNameIndex].h2.string)
-
-				classData = courseInfo[x].find_all('td')
-
-				for y in range(6):
-					content = classData[y].contents
-
-					#unique ID
-					if(y == 0):
-						classUnique.append(content[0].string)
-					#Days
-					elif(y == 1):
-						if(len(content) == 0):
-							classDays.append("None")
-							classDays2.append("None")
-						else:
-							if(content[1] != None):
-								classDays.append(content[1].string)
-								if(content[2].span != None):
-									classDays2.append(content[2].span.string)
-								else:
-									classDays2.append("None")
-
-					#Hour
-					elif(y == 2):
-						if(len(content) == 0):
-							classTime.append("None")
-							classTime2.append("None")
-						else:
-							classTime.append(content[1].string)
-							if(content[2].span != None):
-								classTime2.append(content[2].span.string)
-							else:
-								classTime2.append("None")
-					#Room
-					elif(y == 3):
-						if(len(content) == 0):
-							classRoom.append("None")
-							classRoom2.append("None")
-						else:
-							classRoom.append(content[1].string)
-							if(content[2].span != None):
-								classRoom2.append(content[2].span.string)
-							else:
-								classRoom2.append("None")
-					elif(y == 4):
-						if(len(content) == 0):
-							classInstructor.append("None")
-						else:
-							classInstructor.append(content[0].string)
-					elif(y == 5):
-						classStatus.append(content[0].string)
-
-
+		classAttribute = courseInfo[x].td.get('class')
+		if(classAttribute != None):
+			if(classAttribute[0] == 'course_header'):
+				classNameIndex = x;
 				x += 1
-				if(x == len(courseInfo)):
-					break
 
-				classAttribute = courseInfo[x].td.get('class')
-				if(classAttribute != None ):
-					break
+				while(True):
+					#append class name data
+					className.append(courseInfo[classNameIndex].h2.string)
+
+					classData = courseInfo[x].find_all('td')
+
+					for y in range(6):
+						content = classData[y].contents
+
+						#unique ID
+						if(y == 0):
+							classUnique.append(content[0].string)
+						#Days
+						elif(y == 1):
+							if(len(content) == 0):
+								classDays.append("None")
+								classDays2.append("None")
+							else:
+								if(content[1] != None):
+									classDays.append(content[1].string)
+									if(content[2].span != None):
+										classDays2.append(content[2].span.string)
+									else:
+										classDays2.append("None")
+
+						#Hour
+						elif(y == 2):
+							if(len(content) == 0):
+								classTime.append("None")
+								classTime2.append("None")
+							else:
+								classTime.append(content[1].string)
+								if(content[2].span != None):
+									classTime2.append(content[2].span.string)
+								else:
+									classTime2.append("None")
+						#Room
+						elif(y == 3):
+							if(len(content) == 0):
+								classRoom.append("None")
+								classRoom2.append("None")
+							else:
+								classRoom.append(content[1].string)
+								if(content[2].span != None):
+									classRoom2.append(content[2].span.string)
+								else:
+									classRoom2.append("None")
+
+						elif(y == 4):
+							if(len(content) == 0):
+								classInstructor.append("None")
+							else:
+								classInstructor.append(content[0].string)
+						elif(y == 5):
+							classStatus.append(content[0].string)
+
+
+					x += 1
+					if(x == len(courseInfo)):
+						break
+					if(courseInfo[x].td == None):
+						break
+					classAttribute = courseInfo[x].td.get('class')
+					if(classAttribute != None ):
+						break
 
 
 #write parsed data to file
--- requestTest.py
+++ requestTest.py
@@ -1,16 +1,55 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://utdirect.utexas.edu/apps/registrar/course_schedule/20172/results/?search_type_main=UNIQUE&ccyys=20172&start_unique=00000&unique_number=&end_unique=99999&next_unique=00430"
+#initialize URL offsets
+baseURL = "https://utdirect.utexas.edu/apps/registrar/course_schedule/20172/results/?search_type_main=UNIQUE&ccyys=20172&start_unique=00000&unique_number=&end_unique=99999&next_unique="
+firstPageURL = '00000'
+completeURL = baseURL + firstPageURL
 
-cookie = {'NSC_vue-qspe_443':'ffffffffc3a018e545525d5f4f58455e445a4a42378b',
-          'utlogin-prod':'AQIC5wM2LY4SfcybLi9uC7fWii-qJxtXwNpqNDmey89MYmc.*AAJTSQACMDIAAlMxAAIwMQACU0sAFC0xMDEyMTY1MTU5Nzg0MTUyOTk1*'
+cookie = {'NSC_vue-qspe_443':'ffffffffc3a018e445525d5f4f58455e445a4a42378b',
+          'utlogin-prod':'AQIC5wM2LY4Sfcxb2-Yw2dXfBTakAdUOez4_gmyQqe1dIYs.*AAJTSQACMDIAAlMxAAIwNgACU0sAFC0xMzUwNjQ3NTg1MzcyODUyMTQ3*'
           }
 
-response = requests.get(url, cookies = cookie)
 
-f = open("data.txt", 'w')
-f.write(response.text)
-f.close()
+completeData = open("completeData.txt", 'r+')
 
 
-x = input("d")
+while(True):
+
+	#uses url to get html data and write to completeData
+	response = requests.get(completeURL, cookies = cookie)
+
+	#partial data used to get new URL and is overridden every time
+	partialData = open("partialData.txt", 'r+')
+	partialData.truncate()
+	partialData.write(response.text)
+	partialData.close()
+	
+	
+	
+	completeData.write(response.text)
+	
+	newPartialData = open("partialData.txt", 'r')
+	
+	#generate new URL 
+	index = -1
+	soup = BeautifulSoup(newPartialData, 'html.parser')
+	
+	allLinks = soup.find_all('a')
+	for a in range(len(allLinks)):
+
+		allIDs = allLinks[a].get('id')
+		if(allIDs == 'next_nav_link'):
+			nextPage = allLinks[a].get('href')
+			index = nextPage.find('next_unique') + 12
+			completeURL = baseURL + nextPage[index: index+5]
+			print('Data Downloaded Through Unique: ' + nextPage[index: index+5])
+			break
+
+		#if 'next_nav_link' doesnt exist then terminate
+
+	if index == -1:
+		break;
+
+partialData.close()
+completeData.close()
