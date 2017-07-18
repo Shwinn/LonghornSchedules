7f516698b7ce2959cdcb66385a0a84604b9f2096
unknown <avishkas5@gmail.com>
2017-07-18 18:11:03 -0500

    in the middle of fixing search errors

--- fileserver.js
+++ fileserver.js
@@ -9,6 +9,30 @@ var app = express(); //create new instance of express application
 
 var classData = JSON.parse(fs.readFileSync('jsonData.json', 'utf8'));
 
+//make some changes to data to make searching easier for user
+
+var temp2 = "E E  302 INTRO ELECTRICAL ENGINEERING         ";
+var temp3 = temp2.replace(/E E  /, 'EE ');
+console.log(temp2);
+console.log(temp3);
+
+/*
+for(i = 0; i < classData.length; i++){
+  //make everything lower case
+  var temp1 = classData[i].CourseName;
+  temp1 = temp1.toLowerCase();
+  classData[i].CourseName = temp1;
+
+  //change "E E  " in CourseName to "EE "
+  temp1 = classData[i].CourseName.replace(/E E  /, 'EE ');
+
+  classData[i].CourseName = temp1;
+
+  //change "  " to " " (2 spaces to 1 space)
+  temp1 = classData[i].CourseName.replace(/  +/g, ' ');
+  classData[i].CourseName = temp1;
+}*/
+
 app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(function(req, res, next){
@@ -21,10 +45,10 @@ app.use(express.static("./public"));
 
 app.post("/queryDatabase", function(req, res){
   //convert user input to a reg-exp object
-  var dataRegExp = new RegExp(req.body.searchField);
+  var dataRegExp = new RegExp(req.body.searchField.toLowerCase());
 
   //find matches using query
-  var jsonArray = [];
+  var jsonArray = []; //matching data to be added in here
   for(i = 0; i < classData.length; i++){
     if(dataRegExp.test(classData[i].CourseName)){
       console.log("found!");
--- public/script.js
+++ public/script.js
@@ -3,18 +3,7 @@ $(document).ready(function(){
     $.post("/queryDatabase", $('#classSearch').serialize(), function(data){
       var jsonQuery = JSON.parse(data); //parse queried JSON Data
       console.log(jsonQuery);
+
     });
   });
 });
-
-
-/*
-$(document).ready(function(){
-  $("#button1").click(function(){
-    $.post('/getData', function(data){
-      console.log("getting server side text");
-      $('#insertText').append(data);
-    });
-  });
-});
-*/
