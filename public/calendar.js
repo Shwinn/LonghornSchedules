b80317835fb1e6ac6bd9c59503d80ac0d3820ef0
unknown <avishkas5@gmail.com>
2017-08-15 13:07:33 -0500

    changed certain calendar settings to match needed appearance

--- public/calendar.js
+++ public/calendar.js
@@ -3,6 +3,19 @@ $(document).ready(function(){
     schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
     weekends: false,
     defaultView: 'agendaWeek',
-    minTime: '08:00:00'
+    minTime: '08:00:00',
+    allDaySlot: false,
+
+    events: [
+      {
+        title : 'birthday!',
+        start : '2017-08-14T14:00:00',
+        end : '2017-08-14T16:15:00'
+      }
+    ],
+
+    titleFormat: ' ',
+    columnFormat: 'ddd',
+    buttonIcons: false
   });
 });
